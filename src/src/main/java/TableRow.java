3b41bd5c0090f67bb4f69ac58d82a38fa2a4449b
Javier Palomares <javier_palomares@homedepot.com>
2018-10-18 18:02:35 -0500

    got insert working for one row. Need to figure out how to insert multiple rows

---
+++ src/src/main/java/TableRow.java
@@ -0,0 +1,7 @@
+public class TableRow {
+	public  Integer theKey;
+	public Integer columnA;
+	public Integer columnB;
+	public String filler;
+
+}
--- src/src/main/java/VariationI.java
+++ src/src/main/java/VariationI.java
@@ -1,16 +1,24 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class VariationI
 {
+	private static int NUM_ROWS = 0;
+	private static String SQL = "INSERT INTO benchmark(theKey,columnA,columnB,filler) VALUES(?,?,?,?)";
 	/**
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
 		//Class.forName("org.sqlite.JDBC");
 		Connection connection = connect();
-		insertData(connection);
+		TableRow row = new TableRow();
+		row.theKey = 2;
+		row.columnA = 3;
+		row.columnB = 4;
+		row.filler = "this is a test";
+		insertData(connection, row);
 		if(connection != null){
 			try {
 				connection.close();
@@ -37,7 +45,17 @@ public class VariationI
 		return conn;
 	}
 
-	private static void insertData(Connection conn) {
+	private static void insertData(Connection conn, TableRow row){
+		try{
+			PreparedStatement statement = conn.prepareStatement(SQL);
+			statement.setInt(1,row.theKey);
+			statement.setInt(2,row.columnA);
+			statement.setInt(3,row.columnB);
+			statement.setString(4,row.filler);
+			statement.executeUpdate();
+		}catch (SQLException e){
+			System.err.println(e.getMessage());
+		}
 		/**
 		 * TODO: Complete implementation
 		 */
