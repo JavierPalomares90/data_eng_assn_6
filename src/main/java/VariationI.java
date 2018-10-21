08bc2d60e43c0e8d7840c8323e0e4cdb514d7742
Javier Palomares <javier.palomares.90@gmail.com>
2018-10-20 22:13:59 -0500

    trying to get batch job working

--- src/main/java/VariationI.java
+++ src/main/java/VariationI.java
@@ -7,21 +7,41 @@ import org.apache.commons.lang3.RandomStringUtils;
 
 public class VariationI
 {
-	private static int NUM_ROWS = 50;
+	private static int NUM_ROWS   = 5000000;
+	private static int BATCH_SIZE =  50000;
+	private static int MIN = 1;
+	private static int MAX = 50000;
 	/**
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		String url = "jdbc:sqlite:/Users/PXJ2D5A/Documents/hw3_db.db";
-		//Class.forName("org.sqlite.JDBC");
+		String url = "jdbc:sqlite:C:/Users/javie/Documents/Data_Engineering/hw2.db";
+		// Load the driver
+		try
+		{
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+			System.err.println("Unable to find driver");
+			return;
+		}
 		Connection connection = Utils.connect(url);
 		Utils.createTable(connection);
-		List<TableRow> rows = generateRows();
 		//insertOneRow(connection, row);
-		long startTime = System.nanoTime();
-		Utils.insertBatch(connection,rows);
-		long endTime = System.nanoTime();
-		long executionTime = endTime - startTime;
+		int numLoops = NUM_ROWS / BATCH_SIZE;
+		double processTime = 0.0;
+		for(int i = 0; i < numLoops; i++)
+		{
+			List<TableRow> rows = generateRows(i, BATCH_SIZE);
+			long startTime = System.nanoTime();
+			Utils.insertBatch(connection,rows);
+			long endTime = System.nanoTime();
+			long executionTime = endTime - startTime;
+			processTime += (double) executionTime;
+		}
+		double seconds = (double) processTime / 1000000000.0;
+		System.out.println("Variation I took " + seconds + " to insert " + NUM_ROWS + " rows.");
 		Utils.dropTable(connection);
 		if(connection != null){
 			try {
@@ -33,15 +53,15 @@ public class VariationI
 	}
 
 	// generate and load the rows in sorted order on the primary key.
-	private static List<TableRow> generateRows(){
-		int numRows = NUM_ROWS;
-		int min = 1;
-		int max = 50;
+	private static List<TableRow> generateRows(int n, int batchSize){
+		int numRows = batchSize;
+		int min = MIN;
+		int max = MAX;
 		List<TableRow> rows = new ArrayList<TableRow>();
 		Random r = new Random();
 		for (int i = 0; i < numRows; i++){
 			TableRow row = new TableRow();
-			row.theKey = i;
+			row.theKey = i + n*numRows;
 			// columnA is a random int between 1 and 50
 			row.columnA = r.nextInt((max - min) + 1) + min;
 			row.columnB = r.nextInt((max - min) + 1) + min;
--- src/main/java/VariationII.java
+++ src/main/java/VariationII.java
@@ -8,21 +8,41 @@ import java.util.List;
 import java.util.Random;
 
 public class VariationII {
-	private static int NUM_ROWS = 50;
+	private static int NUM_ROWS   = 5000000;
+	private static int BATCH_SIZE = 50000;
+	private static int MIN = 1;
+	private static int MAX = 50000;
+
 	/**
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		String url = "jdbc:sqlite:/Users/PXJ2D5A/Documents/hw3_db.db";
-		//Class.forName("org.sqlite.JDBC");
+		String url = "jdbc:sqlite:C:/Users/javie/Documents/Data_Engineering/hw2.db";
+		// Load the driver
+		try
+		{
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+			System.err.println("Unable to find driver");
+			return;
+		}
 		Connection connection = Utils.connect(url);
 		Utils.createTable(connection);
-		List<TableRow> rows = generateRows();
-		//insertOneRow(connection, row);
-		long startTime = System.nanoTime();
-		Utils.insertBatch(connection,rows);
-		long endTime = System.nanoTime();
-		long executionTime = endTime - startTime;
+		int numLoops = NUM_ROWS / BATCH_SIZE;
+		double processTime = 0.0;
+		for(int i = 0; i < numLoops; i++)
+		{
+			List<TableRow> rows = generateRows(i, BATCH_SIZE);
+			long startTime = System.nanoTime();
+			Utils.insertBatch(connection,rows);
+			long endTime = System.nanoTime();
+			long executionTime = endTime - startTime;
+			processTime += (double) executionTime;
+		}
+		double seconds = (double) processTime / 1000000000.0;
+		System.out.println("Variation II took " + seconds + " to insert " + NUM_ROWS + " rows.");
 		Utils.dropTable(connection);
 
 		if(connection != null){
@@ -36,13 +56,13 @@ public class VariationII {
 
 	// generate the rows such that the primary key value is chosen at random,
 	// without replacement, from the integers.
-	private static List<TableRow> generateRows(){
-		int numRows = NUM_ROWS;
-		int min = 1;
-		int max = 50;
+	private static List<TableRow> generateRows(int n, int batchSize){
+		int numRows = BATCH_SIZE;
+		int min = MIN;
+		int max = MAX;
 		List<Integer> keys= new ArrayList<Integer>();
 		for(int i = 0; i < numRows; i++){
-			keys.add(i);
+			keys.add(i + n * numRows);
 		}
 
 		Random r = new Random();
